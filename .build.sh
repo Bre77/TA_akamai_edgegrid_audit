61683c12a783f9d2daec38b7ecc4812cd728da42
Brett <brett.whynot@gmail.com>
2024-11-19 09:58:29 +1000

    Remove splunklib before upgrading

--- .build.sh
+++ .build.sh
@@ -4,8 +4,9 @@ OUTPUT="${1:-TA_akamai_edgegrid_audit.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= bin/*
+rm -rf lib/splunk*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
 rm -rf lib/*/__pycache__
 rm -rf lib/*/*/__pycache__
 cd ..
-tar -cpzf $OUTPUT --exclude=.* --overwrite TA_akamai_edgegrid_audit 
+tar -cpzf $OUTPUT --exclude=.* --overwrite TA_akamai_edgegrid_audit
