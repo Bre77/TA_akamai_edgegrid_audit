0fb99e8787bb7479e8b4af75663e1e4f5fcea72a
Brett <brett.whynot@gmail.com>
2024-11-19 10:00:44 +1000

    Handle pycache in edgegrid

--- .build.sh
+++ .build.sh
@@ -8,5 +8,6 @@ rm -rf lib/splunk*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
 rm -rf lib/*/__pycache__
 rm -rf lib/*/*/__pycache__
+rm -rf lib/*/*/*/__pycache__
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --overwrite TA_akamai_edgegrid_audit
